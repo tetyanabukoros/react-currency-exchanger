713a627aec4b84f67c4aa6dddac6559fc67709ea
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-09-26 22:59:01 +0300

    currency exchanger done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState('UAH');
+  const [toCurrency, setToCurrency] = useState('USD');
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(1);
+  // const [rates, setRates] = useState({});
+
+  const ratesRef = useRef({});
+
+  useEffect(() => {
+    fetch('https://cdn.cur.su/api/latest.json')
+      .then(res => res.json())
+      .then((json) => {
+        // setRates(json.rates);
+        ratesRef.current = json.rates;
+        onChangeToPrice(1);
+      })
+      .catch(err => {
+        console.warn(err);
+        alert('Не вдалось отримати інформацію');
+      })
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setToPrice(result.toFixed(2));
+    setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(2));
+    setToPrice(value);
+  };
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
+  
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block 
+        value={fromPrice} 
+        currency={fromCurrency} 
+        onChangeCurrency={setFromCurrency} 
+        onChangeValue={onChangeFromPrice} 
+      />
+
+      <Block 
+        value={toPrice} 
+        currency={toCurrency} 
+        onChangeCurrency={setToCurrency}  
+        onChangeValue={onChangeToPrice} 
+      />
     </div>
   );
 }
